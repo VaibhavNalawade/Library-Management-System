b7ca5c2769d2de74d8fb822fcbb6575503666387
vaibhav nalwade <VaibhavNalawade@outlook.in>
2020-07-20 11:11:04 +0530

    init

---
+++ Library Management System/Controllers/MemberController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library_Management_System.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        [HttpGet]
+       public ActionResult Result()
+        {
+            return Ok(new Member());
+        }
+    }
+}
